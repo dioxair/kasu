52d65dd979dbcb8ba292e5ab9cd4a74d3e0f8df6
Samuel Olagunju <94651716+samuelolagunju@users.noreply.github.com>
2023-06-02 13:02:20 -0400

    Adjusted window dimensions and page zoom

--- src/main.js
+++ src/main.js
@@ -3,8 +3,8 @@ const path = require("path");
 
 const createWindow = () => {
   const win = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1280,
+    height: 720,
     darkTheme: true,
     webPreferences: {
       devTools: true,
