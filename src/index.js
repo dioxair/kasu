b05acffcbe71fb37f7b08869ba19c5b7393a8c7c
Samuel Olagunju <94651716+samuelolagunju@users.noreply.github.com>
2023-06-02 13:02:19 -0400

    Using bot creation method

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 const mineflayer = require("mineflayer");
 
-function onKasuStart() {}
+let bot;
+
+function onKasuStart() {
+  bot = mineflayer.createBot({
+    host: document.getElementById("serverIP").value,
+    port: document.getElementById("serverPort").value,
+    username: document.getElementById("userEmail").value,
+    auth: "microsoft",
+    version: "1.19.3",
+  });
+}
