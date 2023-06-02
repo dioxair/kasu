993185b35f94ff7da1e7ba0c0fb17505f47f29df
Samuel Olagunju <94651716+samuelolagunju@users.noreply.github.com>
2023-06-02 13:02:19 -0400

    Added a status label

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 const mineflayer = require("mineflayer");
+const statusLabel = document.getElementById("statusLabel");
+const sleep = (ms) => new Promise((res) => setTimeout(res, ms));
 
 let bot;
 
 function onKasuStart() {
+  if (
+    document.getElementById("serverIP").value === "" ||
+    document.getElementById("serverPort").value === "" ||
+    document.getElementById("userEmail").value === ""
+  ) {
+    statusLabelError("Please fill out all the forms!");
+    return;
+  } else {
+    statusLabelIdle();
+  }
+
   bot = mineflayer.createBot({
     host: document.getElementById("serverIP").value,
     port: document.getElementById("serverPort").value,
@@ -10,4 +23,21 @@ function onKasuStart() {
     auth: "microsoft",
     version: "1.19.3",
   });
+}
+
+function statusLabelSuccess(text) {
+  statusLabel.textContent = `Status: ${text}`;
+  statusLabel.style.color = "LightGreen";
+}
+function statusLabelWarning(text) {
+  statusLabel.textContent = `Status: ${text}`;
+  statusLabel.style.color = "Khaki";
+}
+function statusLabelError(text) {
+  statusLabel.textContent = `Status: ${text}`;
+  statusLabel.style.color = "LightCoral";
+}
+function statusLabelIdle() {
+  statusLabel.textContent = "Status: Idle";
+  statusLabel.style.color = "peachpuff";
 }
