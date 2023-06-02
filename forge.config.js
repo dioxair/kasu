8fbf975d0537fe97e74b9b80c635d1ae2b98f5dd
Samuel Olagunju <94651716+samuelolagunju@users.noreply.github.com>
2023-06-02 13:02:20 -0400

    Some electron forge configurations

--- forge.config.js
+++ forge.config.js
@@ -1,22 +1,38 @@
 module.exports = {
-  packagerConfig: {},
+  packagerConfig: {
+    icon: "C:\\Users\\Plextora\\source\\repos\\kasu\\out\\Icon stuff\\ICO\\KasuLogo",
+  },
   rebuildConfig: {},
   makers: [
     {
-      name: '@electron-forge/maker-squirrel',
-      config: {},
+      name: "@electron-forge/maker-squirrel",
+      config: {
+        name: "Kasu",
+        iconUrl: "http://files.dioxair.me/r/KasuLogo.ico",
+        setupIcon:
+          "C:\\Users\\Plextora\\source\\repos\\kasu\\out\\Icon stuff\\ICO\\KasuLogo.ico",
+      },
     },
     {
-      name: '@electron-forge/maker-zip',
-      platforms: ['darwin'],
+      name: "@electron-forge/maker-zip",
+      platforms: ["darwin"],
     },
     {
-      name: '@electron-forge/maker-deb',
-      config: {},
+      name: "@electron-forge/maker-deb",
+      config: {
+        options: {
+          maintainer: "Samuel Olagunju",
+          homepage: "https://github.com/dioxair/kasu",
+        },
+      },
     },
     {
-      name: '@electron-forge/maker-rpm',
-      config: {},
+      name: "@electron-forge/maker-rpm",
+      config: {
+        options: {
+          homepage: "https://github.com/dioxair/kasu",
+        },
+      },
     },
   ],
 };
