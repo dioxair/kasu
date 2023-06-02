7e443feab382e0a0e4d4d08df8fff5526774078d
Samuel Olagunju <94651716+samuelolagunju@users.noreply.github.com>
2023-06-02 13:02:18 -0400

    Fix some stuff

--- src/main.js
+++ src/main.js
@@ -1,13 +1,18 @@
 const { app, BrowserWindow } = require("electron");
+const path = require("path");
 
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
+    darkTheme: true,
+    webPreferences: {
+      devTools: true,
+    },
   });
 
-  win.loadFile("index.html");
-  win.removeMenu(true);
+  win.loadFile(path.join(__dirname, "index.html"));
+  win.menuBarVisible(true);
 };
 
 app.whenReady().then(() => {
